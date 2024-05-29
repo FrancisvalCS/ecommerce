1e512ca38fdec56dcd03ce5ad21daa4094c75526
francisval <francisvalcs@gmail.com>
2024-05-29 00:40:10 -0300

    Criando os models Produto e Variação(Fim)

--- produto/models.py
+++ produto/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from PIL import Image
+import os
+from django.conf import settings
+
+
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
@@ -19,6 +24,25 @@ class Produto(models.Model):
         )
     )
 
+    #Redimencionamento de imagem
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+        if original_width <= new_width:
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
+
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
@@ -30,3 +54,16 @@ class Produto(models.Model):
     def __str__(self):
         return self.nome
 
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
